5dc860bba0914d38d717294f523a776aa661d912
Aaron Klump <sourcecode@intheloftstudios.com>
2023-11-26 14:14:09 -0800

    Add multiple php version testing.

---
+++ bin/start_test_server.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+s="${BASH_SOURCE[0]}";[[ "$s" ]] || s="${(%):-%N}";while [ -h "$s" ];do d="$(cd -P "$(dirname "$s")" && pwd)";s="$(readlink "$s")";[[ $s != /* ]] && s="$d/$s";done;__DIR__=$(cd -P "$(dirname "$s")" && pwd)
+
+webroot="$__DIR__/../web"
+php -S 127.0.0.1:8080 -t "$webroot"
